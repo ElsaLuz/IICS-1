ee454c1210e6c36c3ff96840fc563a2ece6be72d
ElsaLuz <elsa.luz198@gmail.com>
2021-05-23 21:22:11 +0500

    Update train_duke.sh

--- script/train_duke.sh
+++ script/train_duke.sh
@@ -1,4 +1,6 @@
 # https://www.geeksforgeeks.org/pythonpath-environment-variable-in-python/ 
-export PYTHONPATH=$PYTHONPATH:./  #for test_duke.sh only argparse flags are changed. Here setting the environment path along with argparse flags to call it like: sh ./script/train_market.sh
-python ./example/iics.py --dataset dukemtmc --checkpoint path_to_checkpoint #It is used to set the path for the user-defined modules so 
+export PYTHONPATH=$PYTHONPATH:./  #for test_duke.sh only command-line arguments are changed. 
+python ./example/iics.py --dataset dukemtmc --checkpoint path_to_checkpoint #1.It is used to set the path for the user-defined modules so 
                                                                             #that it can be directly imported into a Python program.
+                                                                            #2. Command-line arguments being passed to the iics.py script 
+                                                                            #that is being executed by the sh ./script/train_duke.sh shell script
